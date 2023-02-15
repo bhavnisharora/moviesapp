bb9fcc330abf618b7b6ffefc8c64f3ee921b7ad2
bhavnish <bhavnisharora786@gmail.com>
2023-02-15 08:50:12 +0530

    netflix2.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useState } from "react";
+import './App.css'
 function App() {
+
+
+  const [text, settext] = useState("search Movie")
+  const [movie, setmovie] = useState([])
+
+  const changetext = (event) => {
+    settext(event.target.value);
+  }
+
+  const getmovie = (e) => {
+    e.preventDefault();
+    axios.get(`https://www.omdbapi.com/?s=${text}&apikey=e99c1d7c`)
+      .then((response) => {
+        console.log(response);
+        setmovie(response.data.Search)
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+
+      <h1 className="heading_style"> Netflix 2.0</h1>
+      <form action="" onSubmit={getmovie}>
+        <input type='search' placeholder = "search movie" value={text} onChange={changetext} className='search-box'/>
+        <input type="submit"/>
+      </form>
+
+      <div className="container my-3">
+
+        <div className="row">
+
+          {
+            movie.map((value, index) => {
+              return (
+                <>
+                
+                  <div className="col-3">
+                    <div class="cardName" style={{ Width: '18rem' }}>
+                      <img src={value.Poster} className="card-img-top" alt="..." />
+                      <div className="card-body">
+                        <h5 className="card-title">{value.Year}</h5>
+                        <h3 className="card-title">{value.Title}</h3>
+
+                        <a href="/" className="btn btn-danger">click here to watch</a>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )
+            })
+          }
+
+        </div>
+      </div>
+
+    </>
   );
 }
 
