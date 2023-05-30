581eaf22e62b92e676d5d7f605e290b5f7b031b7
kunal <13kunalthakur@gmail.com>
2023-05-30 21:02:24 +0530

    making loading and display errors working done

--- src/practise/Movie.js
+++ src/practise/Movie.js
@@ -1,53 +1,83 @@
-import React, { useState } from 'react'
-import './Movie.css'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import "./Movie.css";
+import axios from "axios";
 const Movie = () => {
-    const [text, settext] = useState(true)
-    const [movie, setmovie] = useState([])
+  const [text, settext] = useState(true);
+  const [movie, setmovie] = useState([]);
+  const [status, setStatus] = useState("");
 
-    const getdata = (e) => {
-        e.preventDefault()
-        axios.get(`https://www.omdbapi.com/?s=${text}&apikey=e99c1d7c`)
-            .then((res) => {
-           
-                console.log(res)
-                setmovie(res.data.Search)
-
-            })
+  const getdata = (e) => {
+    e.preventDefault();
+    setStatus("pending");
+    try {
+        axios
+          .get(`https://www.omdbapi.com/?s=${text}&apikey=e99c1d7c`)
+          .then((res) => {
+            console.log(res);
+            setmovie(res.data.Search);
+          });
+    } catch(err) {
+        console.log(err);
     }
+  };
 
-    const changedata = (e) => {
-        settext(e.target.value)
+  const changedata = (e) => {
+    settext(e.target.value);
+  };
+
+  useEffect(() => {
+    if (movie && movie.length > 0) {
+      setStatus("completed");
     }
+  }, [status, movie]);
+
+  if (status === "pending") {
     return (
-        <>
-            <h1 className='title'>MovieZone</h1>
-            <form action="" onSubmit={getdata} className='set-form'>
-                <input type="text" placeholder="search a movie" className="input-field" onChange={changedata} />
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "grid",
+          placeContent: "center",
+          backgroundColor: "red",
+        }}
+      >
+        <h1 style={{ fontSize: "5rem", color: "black" }}>Loading...</h1>
+      </div>
+    );
+  }
 
-                <input type="submit" value="search" disabled={text.length === 0} />
-            </form>
-            <div className="cards-container">
+  return (
+    <>
+      <h1 className="title">MovieZone</h1>
+      <form action="" onSubmit={getdata} className="set-form">
+        <input
+          type="text"
+          placeholder="search a movie"
+          className="input-field"
+          onChange={changedata}
+        />
 
-                {
-                    movie.map((value) => {
+        <input type="submit" value="search" disabled={text.length === 0} />
+      </form>
+      <div className="cards-container">
+        {movie && movie?.length > 0 && <h3>Here are some results</h3>}
+        {movie?.length > 0 &&
+          movie.map((value, i) => {
+            return (
+              <>
+                <div key={i} className="cards">
+                  <img src={value.Poster} alt="not available" />
+                  <h1>{value.Title.slice(0, 15)}</h1>
+                  <h2 style={{ color: "blue" }}>{value.Year}</h2>
+                </div>
+              </>
+            );
+          })}
+        {!movie && <p style={{ color: "black" }}>No data found!</p>}
+      </div>
+    </>
+  );
+};
 
-                        return (
-                            <>
-                                <div className="cards">
-                                    <img src={value.Poster} alt="not available" />
-                                    <h1>{value.Title.slice(0, 15)}</h1>
-                                    <h2 style={{ color: 'blue' }}>{value.Year}</h2>
-                                </div>
-                            </>
-                        )
-                    })
-                }
-
-            </div>
-          
-        </>
-    )
-}
-
-export default Movie
+export default Movie;
